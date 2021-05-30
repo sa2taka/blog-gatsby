30e58ddcc43ec9dc78a361b3e2f09586a6bef051
sa2taka <sa2taka@gmail.com>
2021-05-31 02:04:29 +0900

    :wrench: delete unneccesary files

---
+++ src/pages/category/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { Layout } from '../../components/layout/layout';
+import { Seo } from '../../components/layout/seo';
+import { useStaticQuery, graphql } from 'gatsby';
+
+interface Props {}
+
+interface CategoryProps {
+  category: GatsbyTypes.AllCategoriesQuery['allContentfulCategory']['edges'][number]['node'];
+}
+
+const Category: React.VFC<CategoryProps> = ({ category }) => {
+  console.log(category);
+  return (
+    <li>
+      {category.name}({category.blog_post ? category.blog_post.length : 0})
+    </li>
+  );
+};
+
+const Categories: React.VFC<Props> = () => {
+  const data = useStaticQuery<GatsbyTypes.AllCategoriesQuery>(graphql`
+    query AllCategories {
+      allContentfulCategory(sort: { fields: sort }) {
+        edges {
+          node {
+            id
+            name
+            slug
+            blog_post {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  console.log(data);
+
+  return (
+    <Layout>
+      <Seo title="カテゴリ"></Seo>
+      <ul>
+        {data.allContentfulCategory.edges
+          .filter(data => data.node.blog_post)
+          .map(data => (
+            <Category category={data.node} key={data.node.id} />
+          ))}
+      </ul>
+    </Layout>
+  );
+};
+
+export default Categories;
